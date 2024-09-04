36bb7c25c0e1fbe7b3b21410bab3955b3fdc5357
maundu55 <marymaundu.mm@gmail.com>
2024-09-04 23:58:06 +0300

    save the todo as an object, check for empty inputs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //SELECT ELEMENTS
 const form = document.getElementById('todoform');
+const todoInput = document.getElementById('newtodo');
 
 //Define an array
 let todos =[];
@@ -13,5 +14,20 @@ form.addEventListener('submit', (e)=>{
 
 //SAVE TODO
 function saveTodo(){
-    
+    const todoValue = todoInput.value;
+
+    //check if the todo is empty
+    const isEmpty = todoValue ==='';
+
+    if(isEmpty){
+        alert()
+    }
+    const todo = {
+        value:todoValue,
+        checked:false,
+        color: '#' + Math.floor(Math.random()*16777215).toString(16)
+    }
+todos.push(todo);
+
+console.log(todos)
 }
